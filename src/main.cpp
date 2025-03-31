16bb7bcaa25a7e586fa7cd750231b92079144297
jaleef <jeffyan167@gmail.com>
2025-03-31 22:47:32 +0800

    在main函数里对ThreadPool类进行测试, 没出现问题

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 // Copyright 2024
+#include <chrono>
+#include <thread>
+
+#include "ThreadPool.h"
 
 int main(int argc, char* argv[]) {
+  ThreadPool pool;
+  pool.start();
+
+  std::this_thread::sleep_for(std::chrono::seconds(5));
   return 0;
 }
