cb741f6e9ceafd24e8338a68844b53f27a9dc2be
jaleef <jeffyan167@gmail.com>
2025-03-31 13:14:53 +0800

    定义了ThreadPool类的所有成员函数

--- include/ThreadPool.h
+++ include/ThreadPool.h
@@ -2,9 +2,19 @@
 #ifndef THREADPOOL_H_
 #define THREADPOOL_H_
 
+#include <vector>
+#include <queue>
+#include <memory>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+
+#include "Thread.h"
+#include "Task.h"
 
 // 线程池支持的模式
-enum ThreadPoolMode {
+enum class ThreadPoolMode {
   MODE_FIXED,  // Fixed thread number
   MODE_CACHED,  // dynamic thread number
 };
@@ -12,7 +22,37 @@ enum ThreadPoolMode {
 
 class ThreadPool {
  public:
+  ThreadPool();
+  ~ThreadPool();
+
+  // 设置线程池的模式
+  void setThreadPoolMode(ThreadPoolMode mode);
+
+  // 设置task任务队列上限阈值
+  void setTaskQueueMaxThreshHold(int threshHold);
+
+  // 给线程池提交任务
+  void submitTask(std::shared_ptr<Task> task);
+
+  // 启动线程池
+  void start();
+
+  ThreadPool(const ThreadPool &) = delete;  // 禁止拷贝构造函数
+  ThreadPool& operator=(const ThreadPool &) = delete;  // 禁止拷贝赋值运算符
+
  private:
+  std::vector<Thread *> threads_{};  // 线程池中的线程列表
+  std::size_t initThreadSize_{};  // 线程池的初始线程数
+
+  std::queue<std::shared_ptr<Task>> taskQueue_{};  // 任务队列
+  std::atomic<int> taskSize_{};  // 任务队列中的任务数
+  int taskQueueMaxThreshHold_{};  // 任务队列的最大阈值
+
+  std::mutex taskQueueMutex_{};  // 任务队列的互斥锁
+  std::condition_variable notFull_{};  // 任务队列非满条件变量
+  std::condition_variable notEmpty_{};  // 任务队列非空条件变量
+
+  ThreadPoolMode poolMode_;  // 线程池的模式
 };
 
 #endif  // THREADPOOL_H_
