baf92a0c9b62d1d21ec321335ffb93921879eff2
jaleef <jeffyan167@gmail.com>
2025-04-01 14:30:18 +0800

    完成了main函数里的测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,47 @@
 // Copyright 2024
 #include <chrono>
+#include <iostream>
 #include <thread>
 
+#include "Task.h"
 #include "ThreadPool.h"
 
+class MyTask : public Task {
+ public:
+  MyTask(int begin, int end) : begin_(begin), end_(end) {}
+
+  // 如何设计run函数的返回值, 可以返回任意类型
+  void run() {
+    std::cout << "begin threadFunc tid: " << std::this_thread::get_id()
+              << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::cout << "end threadFunc tid: " << std::this_thread::get_id()
+              << std::endl;
+  }
+
+ private:
+  int begin_;
+  int end_;
+};
 int main(int argc, char* argv[]) {
   ThreadPool pool;
-  pool.start();
+  pool.start(4);
 
-  std::this_thread::sleep_for(std::chrono::seconds(5));
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+  pool.submitTask(std::make_shared<MyTask>());
+
+  getchar();
   return 0;
 }
