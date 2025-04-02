7dca9aa70df69e0a4bff6ea17780ed43380b882f
jaleef <jeffyan167@gmail.com>
2025-04-02 13:16:47 +0800

    为抽象类Task添加了新的方法

--- include/Task.h
+++ include/Task.h
@@ -2,11 +2,25 @@
 #ifndef TASK_H_
 #define TASK_H_
 
+#include "Any.h"
+
+class Result;
+
 // 任务抽象基类
 class Task {
  public:
+  Task();
+  ~Task() = default;
+
   // 用户可以自定义任意任务类型, 从Task继承, 重写run方法
-  virtual void run() = 0;
+  virtual Any run() = 0;
+
+  void exec();
+
+  void setResult(Result *res);
+
+ private:
+  Result *result_;  // Result对象的生命周期 远大于 Task的
 };
 
 #endif  // TASK_H_
